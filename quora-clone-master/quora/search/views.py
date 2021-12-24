80f6070f17631ec527997d0a730319c70bcd25c9
Aarav Bairagi <aaravbairagi1999@gmail.com>
2021-12-24 16:07:17 +0530

    Changes made in views.py being in branch2

--- quora-clone-master/quora/search/views.py
+++ quora-clone-master/quora/search/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from quora.questions.models import Question
 from django.contrib.auth.decorators import login_required
-
+#This comment is added to play around.
 
 def search(request):
     if 'q' in request.GET:
